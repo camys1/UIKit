c00d6ac19f43b9a4f40d2e66cd2862c903c86e4c
Camelia Samuila <161379354+camys1@users.noreply.github.com>
2025-02-25 16:51:33 +0200

    Add files via upload

---
+++ UIKit-Card Workout/UIKit-Card Workout/CardSelectionVC.swift
@@ -0,0 +1,52 @@
+//
+//  CardSelectionVC.swift
+//  UIKit-Card Workout
+//
+//  Created by Camelia Samuila on 24.02.2025.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+    @IBOutlet var cardImageView: UIImageView!
+    @IBOutlet var Buttons: [UIButton]!
+    
+    var cards: [UIImage] = Card.AllValues
+    
+    var timer: Timer!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startTimer()
+        for button in Buttons {
+            button.layer.cornerRadius = 10
+        }
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+    
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
+    
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+    }
+    
+    
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
+    }
+}
